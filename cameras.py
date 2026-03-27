acaae26a22c59a30fae460b9c48e173e2ca8cb3c
Rutger Meuzelaar <rutger@seascapesubsea.com>
2026-03-27 14:46:00 +0100

    Add supported recording resolutions to camera specs

--- cameras.py
+++ cameras.py
@@ -2,12 +2,26 @@
 This script defines the camera specs
 """
 
+RESOLUTIONS = {
+    "4K": (4096, 2160),
+    "2K": (2560, 1440),
+    "1080p": (1920, 1080),
+    "720p": (1280, 720),
+    "480p": (640, 480) 
+}
+
 class A8MINI:
     MAX_YAW_DEG = 135.0
     MIN_YAW_DEG = -135.0
     MAX_PITCH_DEG = 25.0
     MIN_PITCH_DEG = -90.0
     MAX_ZOOM = 6.0
+    RECORDING_RESOLUTIONS = [
+        RESOLUTIONS['4K'],
+        RESOLUTIONS['2K'],
+        RESOLUTIONS['1080p'],
+        RESOLUTIONS['720p']
+    ]
 
 class ZR10:
     MAX_YAW_DEG = 135.0
@@ -15,3 +29,8 @@ class ZR10:
     MAX_PITCH_DEG = 25.0
     MIN_PITCH_DEG = -90.0
     MAX_ZOOM = 30.0 # 10 optical * 3 digital
+    RECORDING_RESOLUTIONS = [
+        RESOLUTIONS['2K'],
+        RESOLUTIONS['1080p'],
+        RESOLUTIONS['720p']
+    ]
